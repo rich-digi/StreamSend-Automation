daa656ce738371f28d5012753bb23c4f302e139d
Richard Knight <rich@apewave.com>
2015-02-04 16:20:43 +0000

    Updates to support splits

--- gruntfile.js
+++ gruntfile.js
@@ -706,7 +706,14 @@ module.exports = function(grunt)
 	grunt.registerTask('upload-custdata', 'Uploading the Customer Data TSVs to Streamsend', function() {
 		grunt.log.writeln('Uploading the Customer Data TSVs to Streamsend and Initiating Import');
 		imps = {}; // Reset this for each import cycle
-		var files = grunt.file.expand('custdata/processed*.tsv');
+		if (settings.split)
+		{
+			var files = grunt.file.expand('custdata/'+ settings.split +'-split-*.tsv');
+		}
+		else
+		{
+			var files = grunt.file.expand('custdata/processed*.tsv');
+		}
 		console.dir(files);
 		files.forEach(function(f) { 
 			grunt.task.run('exec:upload_custdata:' + f);
